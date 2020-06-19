3b55e3508629c2fed6ea9d1f9f3614726718cae0
Stefan Serkhir <stefan.serkhir@gmail.com>
2020-06-19 19:06:15 +0300

    Created layout item file

--- app/src/main/java/stefanserkhir/randomuserapp/RandomUsersActivity.java
+++ app/src/main/java/stefanserkhir/randomuserapp/RandomUsersActivity.java
@@ -1,10 +1,13 @@
 package stefanserkhir.randomuserapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.io.IOException;
 
@@ -12,10 +15,14 @@ public class RandomUsersActivity extends AppCompatActivity {
 
     private static final String TAG = "RandomUsersActivity";
 
+    private RecyclerView mRandomUsersRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_users);
+
+        mRandomUsersRecyclerView = findViewById(R.id.random_users_recycler_view);
 
         new FetchRandomUsersTask().execute();
     }
