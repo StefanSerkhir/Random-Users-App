17a09fd64cf698dca76d569233d49e6cc5c58955
Stefan Serkhir <stefan.serkhir@gmail.com>
2020-06-19 18:03:39 +0300

    creader model object class

---
+++ app/src/main/java/stefanserkhir/randomuserapp/RandomUser.java
@@ -0,0 +1,49 @@
+package stefanserkhir.randomuserapp;
+
+public class RandomUser {
+    private String mGender;
+    private String mTitle;
+    private String mFirstName;
+    private String mLastName;
+    private String mAvatarURL;
+
+    public String getGender() {
+        return mGender;
+    }
+
+    public void setGender(String gender) {
+        mGender = gender;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String lastName) {
+        mLastName = lastName;
+    }
+
+    public String getAvatarURL() {
+        return mAvatarURL;
+    }
+
+    public void setAvatarURL(String avatarURL) {
+        mAvatarURL = avatarURL;
+    }
+}
