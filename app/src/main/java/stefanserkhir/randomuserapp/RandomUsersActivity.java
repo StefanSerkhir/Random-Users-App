37c88a1e132ce3e48b6e26e7a4063e28f9add279
Stefan Serkhir <stefan.serkhir@gmail.com>
2020-06-20 13:57:21 +0300

    Showed progress bar at bottom

--- app/src/main/java/stefanserkhir/randomuserapp/RandomUsersActivity.java
+++ app/src/main/java/stefanserkhir/randomuserapp/RandomUsersActivity.java
@@ -2,9 +2,12 @@ package stefanserkhir.randomuserapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -20,6 +24,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomUsersActivity extends AppCompatActivity {
+
+    private static final int ITEM_VIEW = 1;
+    private static final int ITEM_PROGRESS = 0;
 
     private RecyclerView mRandomUsersRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
@@ -35,6 +42,7 @@ public class RandomUsersActivity extends AppCompatActivity {
         mLoadingPage = 1;
 
         mRandomUsersRecyclerView = findViewById(R.id.random_users_recycler_view);
+        mRandomUsersRecyclerView.setHasFixedSize(true);
         mLinearLayoutManager = new LinearLayoutManager(this);
         mRandomUsersRecyclerView.setLayoutManager(mLinearLayoutManager);
         mRandomUsersRecyclerView.addOnScrollListener(new ScrollToEndListener());
@@ -91,37 +99,77 @@ public class RandomUsersActivity extends AppCompatActivity {
 
         public void bindRandomUserItem(RandomUser item, int position) {
             mUserNumberTextView.setText(getString(R.string.user_number, position + 1));
+            int rDrawable = item.getGender().equals("male") ?
+                    R.drawable.star_shape : R.drawable.heart_shape;
             Picasso.get()
                     .load(item.getAvatarURL())
                     .transform(new AvatarTransformation(RandomUsersActivity.this,
-                            item.getGender().equals("male") ?
-                                    R.drawable.star_shape : R.drawable.heart_shape))
-                    .placeholder(R.drawable.person_placeholder)
+                            rDrawable))
+                    .placeholder(rDrawable)
                     .into(mUserAvatarImageView);
             mUserFullNameTextView.setText(item.getFullName());
         }
     }
 
-    private class RandomUserAdapter extends RecyclerView.Adapter<RandomUserHolder> {
+    private class ProgressBarHolder extends RecyclerView.ViewHolder {
+        private ProgressBar mProgressBar;
+
+        public ProgressBarHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mProgressBar = itemView.findViewById(R.id.progress_bar);
+        }
+
+        public void bindProgressBarItem() {
+            mProgressBar.getIndeterminateDrawable().setColorFilter(
+                    ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary),
+                    PorterDuff.Mode.MULTIPLY);
+            mProgressBar.setIndeterminate(true);
+        }
+    }
+
+    private class RandomUserAdapter extends RecyclerView.Adapter {
         private List<RandomUser> mRandomUsers;
 
         public RandomUserAdapter(List<RandomUser> randomUsers) {
             mRandomUsers = randomUsers;
         }
 
+        @Override
+        public int getItemViewType(int position) {
+            return (position + 1) != mRandomUsers.size() ? ITEM_VIEW : ITEM_PROGRESS;
+        }
+
         @NonNull
         @Override
-        public RandomUserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View itemView = getLayoutInflater().inflate(R.layout.random_user_item,
-                                                            parent, false);
-            return new RandomUserHolder(itemView);
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            if (viewType == ITEM_VIEW) {
+                View itemView = getLayoutInflater().inflate(R.layout.random_user_item,
+                        parent, false);
+                return new RandomUserHolder(itemView);
+            } else {
+                View itemView = getLayoutInflater().inflate(R.layout.progress_bar, parent, false);
+                return new ProgressBarHolder(itemView);
+            }
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RandomUserHolder randomUserHolder, int position) {
-            RandomUser randomUser = mRandomUsers.get(position);
-            randomUserHolder.bindRandomUserItem(randomUser, position);
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            if (holder instanceof RandomUserHolder) {
+                RandomUserHolder randomUserHolder = (RandomUserHolder) holder;
+                RandomUser randomUser = mRandomUsers.get(position);
+                randomUserHolder.bindRandomUserItem(randomUser, position);
+            } else {
+                ProgressBarHolder progressBarHolder = (ProgressBarHolder) holder;
+                progressBarHolder.bindProgressBarItem();
+            }
         }
+//
+//        @Override
+//        public void onBindViewHolder(@NonNull RandomUserHolder randomUserHolder, int position) {
+//            RandomUser randomUser = mRandomUsers.get(position);
+//            randomUserHolder.bindRandomUserItem(randomUser, position);
+//        }
 
         @Override
         public int getItemCount() {
